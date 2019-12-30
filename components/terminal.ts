af47fe84bb661989755c0fbea15e4f82b33b37d9
Alaa <9027148+alaabenfatma@users.noreply.github.com>
2019-12-30 20:06:08 +0100

    Feature: append lines : DONE.

--- components/terminal.ts
+++ components/terminal.ts
@@ -1,15 +1,34 @@
+let path: String;
+let number_of_cmds: number = 0;
+let username: String = "user" + random(1, 30);
+let table: HTMLTableElement;
 
 function random(x, y) {
     return Math.floor(Math.random() * (y - x + 1) + x);
 }
-let path: String;
-let number_of_cmds: number = -1;
-let username: String = "user" + random(1, 30);
-let table: HTMLTableElement;
+window.onload = function () {
+    CT = new Terminal();
+}
 
 class  Command{
    
     input_cmd: HTMLInputElement;
+
+    getElement(){
+        return this.input_cmd; 
+    }
+    exec(){
+        let reflected_msg: string = this.input_cmd.value;
+        let output: HTMLParagraphElement = document.createElement('p');
+        output.classList.add("consolas");
+        output.innerText = reflected_msg;
+        var row = table.insertRow(1);
+        let output_container = row.insertCell(0);
+        output_container.innerHTML = '<td ></td>';
+        output_container.appendChild(output);
+        this.input_cmd.disabled = true;
+        
+        }
     constructor(){
         this.input_cmd = document.createElement('input');
         this.input_cmd.classList.add('terminal_input');
@@ -17,20 +36,19 @@ class  Command{
         this.input_cmd.type = "text";
         this.input_cmd.autofocus = true;
         this.input_cmd.addEventListener('keypress', (e: KeyboardEvent) =>{
-            //You have yout key code here
              if(e.keyCode == 13){
-                 CT.insertLine();
+                 this.exec();
+                 
+                 CT = new Terminal();
+                 (document.getElementById('cmd' + (number_of_cmds).toString()) as HTMLInputElement).focus();
              }
         
          })
+         this.input_cmd.focus();
     }
     
-    getElement(){
-        return this.input_cmd; 
-    }
 }
 class Terminal {
-
     constructor() {
         number_of_cmds++;
         table = document.createElement('table');
@@ -38,7 +56,7 @@ class Terminal {
         table.cellSpacing = "0";
         table.setAttribute("id", "Terminal");
         document.body.appendChild(table);
-        var row = table.insertRow(number_of_cmds);
+        var row = table.insertRow(0);
         var user_host_cell = row.insertCell(row.cells.length);
         let user_host = '<td >user@cheese:</td>';
         user_host_cell.innerHTML = user_host;
@@ -58,39 +76,11 @@ class Terminal {
         cmd_cell.width = "fit-contents";
         cmd_cell.innerHTML = cmd_host;
         cmd_cell.appendChild(new Command().getElement())
-    }
-    insertLine() {
-        number_of_cmds++;
-        var row = table.insertRow(number_of_cmds);
-        var user_host_cell = row.insertCell(row.cells.length);
-        let user_host = '<td>user@cheese:</td>';
-        user_host_cell.innerHTML = user_host;
-        user_host_cell.classList.add("td_user_host_dollar")
 
-        var path_cell = row.insertCell(row.cells.length);
-        let path = '<td>~</td>';
-        path_cell.innerHTML = path;
-        path_cell.classList.add("td_path")
-
-        var dollar_cell = row.insertCell(row.cells.length);
-        let dollar_host = '<td>🧀 </td>';
-        dollar_cell.innerHTML = dollar_host;
-
-        var cmd_cell = row.insertCell(row.cells.length);
-        let cmd_host = '<td style="width: fit-contents;"></td>';
-        cmd_cell.width = "fit-contents";
-        cmd_cell.innerHTML = cmd_host;
-        cmd_cell.appendChild(new Command().getElement())
 
     }
 }
 
 //Cheese Terminal 
 let CT: Terminal;
-window.onload = function () {
-    CT = new Terminal();
-}
 
-function addNewCmd() {
-    CT.insertLine();
-}
